5205722f40c27c9c35a806eb48122c4259e23a35
duongtanton <20120598@student.hcmus.edu.vn>
2023-05-18 21:17:20 +0700

    Add ony init code project

---
+++ sources/StudentsManagement/src/test/java/com/tkpm/studentsmanagement/StudentsManagement/StudentsManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tkpm.studentsmanagement.StudentsManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentsManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
