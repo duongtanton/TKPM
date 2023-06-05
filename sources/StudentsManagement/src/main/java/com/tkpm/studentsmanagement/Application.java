04c0a886a3a42ab01a2f3d5f865c3fdd67beeee2
HoangPhuc0304 <98740174+HoangPhuc0304@users.noreply.github.com>
2023-06-05 15:17:09 +0700

    refactor: update mail service structure

--- sources/StudentsManagement/src/main/java/com/tkpm/studentsmanagement/Application.java
+++ sources/StudentsManagement/src/main/java/com/tkpm/studentsmanagement/Application.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.security.crypto.encrypt.Encryptors;
 import org.springframework.security.crypto.encrypt.TextEncryptor;
 
@@ -14,6 +15,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 // @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 @SpringBootApplication
+@EnableAsync
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
