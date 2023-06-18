5cb77b3521ed01d2c519b8851a41b5f65611750f
duongtanton <20120598@student.hcmus.edu.vn>
2023-06-18 14:09:10 +0700

    change env

--- sources/StudentsManagement/src/main/java/com/tkpm/studentsmanagement/Application.java
+++ sources/StudentsManagement/src/main/java/com/tkpm/studentsmanagement/Application.java
@@ -15,6 +15,7 @@ import org.modelmapper.spi.MappingContext;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.security.crypto.encrypt.Encryptors;
